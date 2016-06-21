1e6f0eb82b075ab3a1e554d367a848af6fb2b700
dubbinary <dubbinary@gmail.com>
2016-06-21 20:33:58 +0300

    Added implementation of compressing images in RGB

--- cli_dispatcher.py
+++ cli_dispatcher.py
@@ -6,21 +6,37 @@ warnings.filterwarnings("ignore")
 
 working_dir = os.getcwd()
 img_path = None
+help_msg = '\n#For help:\n' \
+           '> wvlt.py -h\n' \
+           '\n#Encode picture:\n' \
+           '> wvlt.py -e <inputfile> [color_mode, mode, threshold]\n' \
+           '\n#Decode picture:\n' \
+           '> wvlt.py -d <inputfile> [color_mode, mode]\n' \
+           '\n#Compess picture:\n' \
+           '> wvlt.py -c <inputfile> [color_mode, mode, threshold]\n' \
+           '\n#Merge two pictures with different compression rate:\n' \
+           '> wvlt.py -m <inputfile1> [<inputfile2>, color_mode, mode, threshold_1, threshold_2]\n' \
+           '\n#Save result to file:\n' \
+           '> wvlt.py -[edcm] <inputfile1> -s <outputfile> [<inputfile2>, color_mode, mode, threshold_1, threshold_2]  \n' \
+           '\n#Use verbose mod:\n' \
+           'wvlt.py -[vedcm]\n'
 
 
 def cdisp(argv):
     try:
-        opts, args = getopt.getopt(argv, "e:d:c:m:g:s:v", ["file=", "outfile="])
-        # print(args)
-        # print(opts)
+        opts, args = getopt.getopt(argv, "e:d:c:m:g:s:v", ["encode=", "decode=", "compress=", "merge=", "save="])
+        print(opts)
+        print(args)
+        if len(opts) == 0:
+            print(help_msg)
     except getopt.GetoptError:
-        print('*** wvlt.py -[ed] <inputfile> -s <outputfile>')
+        print(help_msg)
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('wvlt.py -[ed] <inputfile> -s <outputfile>')
+            print(help_msg)
             sys.exit()
-        elif opt in ("-e", "--file"):   #encode
+        elif opt in ("-e", "--encode"):   #encode
             if arg == '':
                 img_path = choose_image()
             else:
@@ -29,7 +45,7 @@ def cdisp(argv):
             mode = args[1] if len(args) > 1 else "D2"
             threshold = float(args[2]) if len(args) > 2 else 0.05
             img = core.encode(img_path, color_mode, mode, threshold)
-        elif opt in ("-d", "--file"):   #decode not works\\\\\\\\\\\\\\\\\\
+        elif opt in ("-d", "--decode"):   #decode not works\\\\\\\\\\\\\\\\\\
             if arg == '':
                 img_path = choose_image()
             else:
@@ -38,7 +54,7 @@ def cdisp(argv):
             mode = args[1] if len(args) > 1 else "D2"
 
             img = core.decode(img_path, color_mode, mode)
-        elif opt in ("-c", "--file"):
+        elif opt in ("-c", "--compress"):
             if arg == '':
                 img_path = choose_image()
             else:
@@ -48,16 +64,16 @@ def cdisp(argv):
             threshold = float(args[2]) if len(args) > 2 else 0.05
             encoded_img = core.encode(img_path, color_mode, mode, threshold)
             img = core.decode(encoded_img,color_mode,mode)
-        elif opt in ("-m", "--file"):   #merge
+        elif opt in ("-m", "--merge"):   #merge
             img_path_1 = arg
             img_path_2 = args[0]
             color_mode = args[1] if len(args) > 1 else "F"
             mode = args[2] if len(args) > 2 else "D2"
-            threshold_1 = float(args[3]) if len(args) > 3 else 0.05
-            threshold_2 = float(args[4]) if len(args) > 4 else 0.05
+            threshold_1 = float(args[3]) if len(args) > 3 else 0.01
+            threshold_2 = float(args[4]) if len(args) > 3 else 0.01
             encoded_img_1 = core.encode(img_path_1, color_mode, mode, threshold_1)
             encoded_img_2 = core.encode(img_path_2, color_mode, mode, threshold_2)
-            img = core.merge(encoded_img_1, encoded_img_2, mode)
+            img = core.compression_merge(encoded_img_1, encoded_img_2, mode)
         elif opt in ("-g", "--file"):
             if arg == '':
                 img_path = choose_image()
@@ -68,7 +84,7 @@ def cdisp(argv):
             threshold = float(args[2]) if len(args) > 2 else 0.05
             encoded_img = core.exp(img_path, color_mode, mode, threshold)
             # img = core.decode(encoded_img, color_mode, mode)
-        elif opt in ("-s", "--file"):   #save as image
+        elif opt in ("-s", "--save"):   #save as image
             core.save(img, arg)
         elif opt == "-v":   #verbose
             core.verbose = True
--- core.py
+++ core.py
@@ -4,25 +4,20 @@ import sys
 from math import sqrt
 
 verbose = False
-sizes = [2 ** i for i in range(1, 12)]
+sizes = [2 ** i for i in range(1, 12)]  # Image sizes given by power of 2
 
 CL_D2 = [1/sqrt(2), 1/sqrt(2)]              # D2 coefficients of low frequency filter
 CL_D4 = [(1 + sqrt(3)) / (4 * sqrt(2)),     # D4 coefficients of low frequency filter
         (3 + sqrt(3)) / (4 * sqrt(2)),
         (3 - sqrt(3)) / (4 * sqrt(2)),
         (1 - sqrt(3)) / (4 * sqrt(2))]
-CL_D6 = [   0.47046721 / sqrt(2),  # D6 coefficients of low frequency filter
-            1.14111692 / sqrt(2),
-            0.650365 / sqrt(2),
-            -0.19093442 / sqrt(2),
-            -0.12083221 / sqrt(2),
-            0.0498175 / sqrt(2),]
-
 
 def get_hpf_coeffs(CL):            # Coefficients of high frequency filter
+    """ Return list with coefficients of high frequency filter """
     N = len(CL)                    # The number of coefficients
     CH = [(-1)**k * CL[N - k - 1]  # The coefficients in reverse order with alternating sign
         for k in range(N)]
+
     return CH
 
 def pconv(data, CL, CH, delta = 0):
@@ -38,6 +33,7 @@ def pconv(data, CL, CH, delta = 0):
             sH += data[(k + i - delta) % M] * CH[i]
         out.append(sL)
         out.append(sH)
+
     return out
 
 
@@ -49,31 +45,37 @@ def get_icoeffs(CL, CH):
     for k in range(0, len(CL), 2):
         iCL.extend([CL[k-2], CH[k-2]])
         iCH.extend([CL[k-1], CH[k-1]])
+
     return (iCL, iCH)
 
 
 def dwt2(image, CL):
     CH = get_hpf_coeffs(CL)     # Calculate the missing coefficients
     w, h = image.shape
-    imageT = image.copy()       # Copy the original image to convert
+    # imageT = image.copy()       # Copy the original image to convert ######################
+    imageT = image
     for i in range(h):          # Process the lines
         imageT[i, :] = pconv(imageT[i, :], CL, CH)
     # print(imageT)
     for i in range(w):          # Process the columns
         imageT[:, i] = pconv(imageT[:, i], CL, CH)
     # print(imageT)
-    data = imageT.copy()
+
+    # data = imageT
+    data = imageT.copy()  ##########################BAD
     data[0:h / 2, 0:w / 2] = imageT[0:h:2, 0:w:2]   # top-left
     data[h / 2:h, 0:w / 2] = imageT[1:h:2, 0:w:2]   # bottom-left
     data[0:h / 2, w / 2:w] = imageT[0:h:2, 1:w:2]   # top-right
     data[h / 2:h, w / 2:w] = imageT[1:h:2, 1:w:2]   # bottom-right
+
     return data
 
 def idwt2(data, CL):
     w, h = data.shape
 
     # Rearrange the columns and rows back
-    imageT = data.copy()
+    # imageT = data
+    imageT = data.copy()  #####################
     imageT[0:h:2, 0:w:2] = data[0:h/2, 0:w/2]
     imageT[1:h:2, 0:w:2] = data[h/2:h, 0:w/2]
     imageT[0:h:2, 1:w:2] = data[0:h/2, w/2:w]
@@ -84,24 +86,21 @@ def idwt2(data, CL):
     image = imageT.copy()               # Copy the original image to convert
     for i in range(w):                  # Process the columns
         image[:, i] = pconv(image[:, i], iCL, iCH, delta=len(iCL)-2)
-    # print(image)
+
     for i in range(h):                  # Process the lines
         image[i, :] = pconv(image[i, :], iCL, iCH, delta=len(iCL)-2)
-    # print(image)
 
     return image
 
 
-def open_image(path, mode, color_mode):
-    image = Image.open(path, mode).convert(color_mode)
-    im_width = image.width
-    im_heigh = image.height
-    if im_heigh != im_width or im_width not in sizes:
-        image = adapt_img_size(image)
-    image_array = np.array(image)
-    print(image)
+def open_image_as_array(path, open_mode, color_mode):
+    image = adapt_img_size(Image.open(path, open_mode).convert(color_mode))
+    if is_rgb_color_mode(color_mode):
+        image_ch = np.array(get_channels(image))
+    else:
+        image_ch = np.array(image)
     image.close
-    return image_array
+    return image_ch
 
 
 def adapt_img_size(img):
@@ -128,28 +127,49 @@ def encode(path, color_mode="F", mode="D2", threshold=0.05):
     print("Mode: " + mode)
     print("Treshhold: " + str(threshold))
 
-    image_arr = open_image(path, "r", color_mode)
-    if verbose: Image._show(Image.fromarray(image_arr, color_mode))
+    image_ch = open_image_as_array(path, "r", color_mode)
+
     CL = check_mode(mode)
+    if is_rgb_color_mode(color_mode):
+        encoded_img = encoding_rgb(image_ch, CL, threshold)
+    else:
+        encoded_img = encoding_grayscale(image_ch, CL, threshold, color_mode)
 
-    data = image_arr.copy()/255
-    print(CL)
+    print("--- Encoding DONE ---")
+    return encoded_img
 
+def encoding_grayscale(image_ch, CL, threshold, color_mode):
+    data = image_ch / 255
     w, h = data.shape
     while w >= len(CL) and h >= len(CL):
         data[0:w, 0:h] = dwt2(data[0:w, 0:h], CL)
         w /= 2
         h /= 2
-
-    # data = dwt2(data, CL)
-    if verbose: Image._show(Image.fromarray(data * 255, color_mode))
-    encoded_img = data
+    show_image_from_nmlz_data(data, color_mode)
     print("Quantization with parameter "+str(threshold)+" ...")
-    encoded_img[abs(data) < threshold] = 0
-    # print(encoded_img*255)
-    encoded_img = Image.fromarray(encoded_img * 255, color_mode)
-    if verbose: Image._show(encoded_img)
-    print("--- Encoding DONE ---")
+    data[abs(data) < threshold] = 0
+    show_image_from_nmlz_data(data, color_mode)
+    encoded_img = Image.fromarray(data * 255, color_mode)
+    return encoded_img
+
+def encoding_rgb(image_ch, CL, threshold):
+    image_ch = [image_ch[0] / 255, image_ch[1] / 255, image_ch[2] / 255]
+    w = len(image_ch[0][0])
+    h = len(image_ch[0])
+    while w >= len(CL) and h >= len(CL):
+        image_ch[0][0:w, 0:h] = dwt2(image_ch[0][0:w, 0:h], CL)
+        image_ch[1][0:w, 0:h] = dwt2(image_ch[1][0:w, 0:h], CL)
+        image_ch[2][0:w, 0:h] = dwt2(image_ch[2][0:w, 0:h], CL)
+        w /= 2
+        h /= 2
+    show_image_from_nmlz_data(image_ch, color_mode="RGB")
+    print("Quantization with parameter " + str(threshold) + " ...")
+    image_ch[0][abs(image_ch[0]) < threshold] = 0
+    image_ch[1][abs(image_ch[1]) < threshold] = 0
+    image_ch[2][abs(image_ch[2]) < threshold] = 0
+    show_image_from_nmlz_data(image_ch, color_mode="RGB")
+    ch = [image_ch[0] * 255, image_ch[1] * 255, image_ch[2] * 255]
+    encoded_img = image_from_rgb_channels(ch[0], ch[1], ch[2])
     return encoded_img
 
 def decode(path, color_mode="F", mode="D2"):
@@ -158,32 +178,60 @@ def decode(path, color_mode="F", mode="D2"):
     print("Mode: " + mode)
 
     if isinstance(path, str):
-        image_arr = open_image(path, "r", color_mode)
+        image_ch = open_image_as_array(path, "r", color_mode)
     else:
-        image_arr = np.array(path)
+        if is_rgb_color_mode(color_mode):
+            image_ch = np.array(get_channels(path))
+        else:
+            image_ch = np.array(path)
         color_mode = path.mode
-    # print(image_arr)
+    show_image_from_nmlz_data(image_ch, color_mode)
     CL = check_mode(mode)
-    if verbose: Image._show(Image.fromarray(image_arr, color_mode))
-    data = image_arr.copy() / 255
-    print(CL)
-    # print(data)
 
+    if is_rgb_color_mode(color_mode):
+        decoded_img = decode_rgb(image_ch, CL)
+    else:
+        decoded_img = decode_grayscale(image_ch, CL, color_mode)
+    Image._show(decoded_img)
+    print("--- Decoding DONE ---")
+    return decoded_img
+
+def decode_grayscale(image_ch, CL, color_mode):
+    data = image_ch / 255
     im_width, im_heigh = data.shape
     w = h = len(CL)
     while w <= im_width and h <= im_heigh:
         data[0:w, 0:h] = idwt2(data[0:w, 0:h], CL)
         w *= 2
         h *= 2
-    # data = idwt2(data, CL)
-    # print(data*255)
+
     decoded_img = Image.fromarray(data * 255, color_mode)
-    if verbose: Image._show(decoded_img)
-    print("--- Decoding DONE ---")
+    # if verbose: Image._show(decoded_img)
     return decoded_img
 
-def merge(img1, img2, mode):
+def decode_rgb(image_ch, CL):
+    image_ch = [image_ch[0] / 255, image_ch[1] / 255, image_ch[2] / 255]
+    im_width, im_heigh = len(image_ch[0][0]), len(image_ch[0])
+    w = h = len(CL)
+    while w <= im_width and h <= im_heigh:
+        image_ch[0][0:w, 0:h] = idwt2(image_ch[0][0:w, 0:h], CL)
+        image_ch[1][0:w, 0:h] = idwt2(image_ch[1][0:w, 0:h], CL)
+        image_ch[2][0:w, 0:h] = idwt2(image_ch[2][0:w, 0:h], CL)
+        w *= 2
+        h *= 2
+    show_image_from_nmlz_data(image_ch, color_mode="RGB")
+    image_ch = [image_ch[0] * 255, image_ch[1] * 255, image_ch[2] * 255]
+    decoded_img = image_from_rgb_channels(image_ch[0], image_ch[1], image_ch[2])
+    return decoded_img
+
+def compression_merge(img1, img2, mode):
     color_mode = img1.mode
+    print('IMG1: '+str(img1.width))
+    print('IMG2: ' + str(img2.width))
+    if img1.width < img2.width:
+        img2 = img2.resize((img1.width, img1.width))
+    else:
+        img1 = img1.resize((img2.width, img2.width))
     image1_arr = np.array(img1)
     image2_arr = np.array(img2)
     img=Image.fromarray(image1_arr+image2_arr, color_mode)
@@ -196,77 +244,97 @@ def exp(path, color_mode="F", mode="D2", threshold=0.05):
     print("Color mode: " + color_mode)
     print("Mode: " + mode)
     print("Treshhold: " + str(threshold))
-    img= Image.open(path, "r")
-    Image._show(img)
-    img_arr = np.array(img)
-    ch = get_channels(img)
-    Image._show(Image.fromarray(np.array(img_arr), color_mode))
-    # print(ch)
-    # print(image_arr)
-    # Image._show(Image.fromarray(np.array(ch), color_mode))
-    # data = image_arr.copy() / 255
-    # print(data)
-    # Image._show(Image.fromarray(data, color_mode))
-    print()
-    # Image._show(Image.fromarray(data*255, color_mode))
-    # image_arr = open_image(path, "r", color_mode)
-    # if verbose: Image._show(Image.fromarray(image_arr, color_mode))
-    # CL = check_mode(mode)
-    #
-    # data = image_arr.copy() / 255
-    # print(CL)
-    #
-    # w, h = data.shape
-    # while w >= len(CL) and h >= len(CL):
-    #     data[0:w, 0:h] = dwt2(data[0:w, 0:h], CL)
-    #     w /= 2
-    #     h /= 2
-    #
-    # # data = dwt2(data, CL)
-    # if verbose: Image._show(Image.fromarray(data * 255, color_mode))
-    # encoded_img = data
-    # print("Quantization with parameter " + str(threshold) + " ...")
-    # encoded_img[abs(data) < threshold] = 0
-    # # print(encoded_img*255)
-    # encoded_img = Image.fromarray(encoded_img * 255, color_mode)
-    # if verbose: Image._show(encoded_img)
+
+    img = adapt_img_size(Image.open(path, "r").convert("RGB"))
+    ch = np.array(get_channels(img))
+
+    #############ENCODE
+
+    CL = check_mode(mode)
+    ch = [ch[0]/255,ch[1]/255,ch[2]/255 ]
+
+    w = len(ch[0][0])
+    h = len(ch[0])
+    while w >= len(CL) and h >= len(CL):
+        ch[0][0:w, 0:h] = dwt2(ch[0][0:w, 0:h], CL)
+        ch[1][0:w, 0:h] = dwt2(ch[1][0:w, 0:h], CL)
+        ch[2][0:w, 0:h] = dwt2(ch[2][0:w, 0:h], CL)
+        w /= 2
+        h /= 2
+    show_image_from_nmlz_data(ch, color_mode)
+    print("Quantization with parameter " + str(threshold) + " ...")
+    ch[0][abs(ch[0]) < threshold] = 0
+    ch[1][abs(ch[1]) < threshold] = 0
+    ch[2][abs(ch[2]) < threshold] = 0
+    show_image_from_nmlz_data(ch, color_mode)
     print("--- Encoding DONE ---")
 
-def get_channels(img):
-    w = img.width
-    h = img.height
-    print("Width: "+str(w)+" \nHeight: "+str(h))
+    ##############################DECODE
+    im_width, im_heigh = len(ch[0][0]),len(ch[0])
+    w = h = len(CL)
+    while w <= im_width and h <= im_heigh:
+        ch[0][0:w, 0:h] = idwt2(ch[0][0:w, 0:h], CL)
+        ch[1][0:w, 0:h] = idwt2(ch[1][0:w, 0:h], CL)
+        ch[2][0:w, 0:h] = idwt2(ch[2][0:w, 0:h], CL)
+        w *= 2
+        h *= 2
+
+    ch = [ch[0] * 255, ch[1] * 255, ch[2] * 255]
+    Image._show(image_from_rgb_channels(ch[0], ch[1], ch[2]))
+
+def get_channels(image):
+    """ Return list of RGB channels from image """
+    w = image.width
+    h = image.height
     red_ch = [[0 for i in range(w)] for i in range(h)]
     green_ch = [[0 for i in range(w)] for i in range(h)]
     blue_ch = [[0 for i in range(w)] for i in range(h)]
     for i in range(0, w):
-        for j in range(i, h):
-            r,g,b = img.getpixel((i, j))
+        for j in range(0, h):
+            r,g,b = image.getpixel((i, j))
             red_ch[i][j] = r
             green_ch[i][j] = g
-            blue_ch[i][j] = g
-    # print(np.array(red_ch))
-    # Image._show(Image.fromarray(np.array(red_ch) , "L"))
-    # Image._show(Image.fromarray(np.array(green_ch), "L"))
-    # Image._show(Image.fromarray(np.array(blue_ch), "L"))
-    return (red_ch, green_ch, blue_ch)
+            blue_ch[i][j] = b
+    return [red_ch, green_ch, blue_ch]
+
+def image_from_rgb_channels(red_ch, green_ch, blue_ch):
+    """ Return object of Image from RGB channels """
+    w = len(red_ch[0])
+    h = len(red_ch)
+    img = Image.new("RGB", (w,h))
+    for i in range(0, w):
+        for j in range(0, h):
+            img.putpixel((i,j),(int(red_ch[i][j]), int(green_ch[i][j]), int(blue_ch[i][j])))
+    return img
 
 def check_mode(mode):
+    """ Return list with matched coefficients of low frequency filter """
     if mode == "D2":
         CL = CL_D2
     elif mode == "D4":
         CL = CL_D4
-    elif mode == "D6":
-        CL = CL_D6
     else:
         print("*** Unknown encode mod: " + mode + " ***")
         sys.exit(1)
     return CL
 
+def is_rgb_color_mode(color_mode):
+    if color_mode == "RGB":
+        return True
+    else:
+        return False
 
+def show_image_from_nmlz_data(data, color_mode):
+    if not verbose: return
+    if is_rgb_color_mode(color_mode):
+        ch = [data[0] * 255, data[1] * 255, data[2] * 255]
+        Image._show(image_from_rgb_channels(ch[0], ch[1], ch[2]))
+    else:
+        Image._show(Image.fromarray(data * 255, color_mode))
 
 
 def save(img, path):
+    """ Save image on disk by path """
     print("Saving to "+str(path)+" ...")
     img = img.convert("L")
     img.save(path)
